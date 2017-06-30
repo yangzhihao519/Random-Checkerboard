44cdb33948e1b5caef539cba06e17a11816e9cd1
Zhihao Yang <yang_zhihao@live.com>
2017-06-30 18:28:09 +0100

    Generate several sets of patterns at one time

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Your JS goes here
+// CONFIGURATION
+// Size of the checkboard
+var rowNumber = 8; // also the number of columns
+var gridNumber = rowNumber * rowNumber;
+// Number of the set of image to be generated
+var setNumber = 3;
 
-var getBody = document.getElementsByTagName("body");
-
-// Generate Yellow or Red Div
+// Funtions to generate Yellow or Red Div
 function getPercentageNumber(number){
     return (Math.floor(number*10000)/100).toString() + "%";
 }
 
-var rowNumber = 8; // also the number of columns
-var gridNumber = rowNumber * rowNumber;
-var percentageNumber = getPercentageNumber(1/rowNumber);
+// Width of the checkerboard: in percentage of the window's width
+var percentageNumber = getPercentageNumber(1/rowNumber); 
 
 function createYellowDiv(parentDiv) {
     var createYellowDiv = document.createElement("div");
@@ -29,71 +31,75 @@ function createRedDiv(parentDiv) {
     createRedDiv.style.float = "left"
 }
 
-// Randomise the color of the cubes
+// Generate random numbers to randomise the color of the cubes
 function getRandomNumber(){
     return Math.floor(Math.random() * 11);
 }
 
-var randomNumberStack = [];
+// Start the generate patterns
+for(let n = 0; n < setNumber; n++){
+    var randomNumberStack = [];
 
-for (let x = 0; x < gridNumber; x++) {
-    var r = getRandomNumber();
-    randomNumberStack.push(r);
-}
-
-var firstPatternDiv = document.createElement("div");
-firstPatternDiv.className="pattern";
-firstPatternDiv.id="first-pattern";
-document.body.appendChild(firstPatternDiv);
-
-for(i in randomNumberStack){
-    if (randomNumberStack[i] % 3 == 0) {
-        createRedDiv(firstPatternDiv); 
-    } else {
-        createYellowDiv(firstPatternDiv);
+    for (let x = 0; x < gridNumber; x++) {
+        var r = getRandomNumber();
+        randomNumberStack.push(r);
     }
-}
 
-html2canvas(firstPatternDiv, {
-    onrendered: function (canvas) {
-        var a = document.createElement('a');
-        a.href = canvas.toDataURL("image/png");
-        a.download = 'test1.jpg';
-        a.click();
-    }
-});
+    var firstPatternDiv = document.createElement("div");
+    firstPatternDiv.className="pattern";
+    firstPatternDiv.id="first-pattern-"+n;
+    document.body.appendChild(firstPatternDiv);
 
-var randomChangeNumber = Math.floor(Math.random() * gridNumber);
-
-var secondPatternDiv = document.createElement("div");
-secondPatternDiv.className="pattern";
-secondPatternDiv.id="second-pattern";
-document.body.appendChild(secondPatternDiv);
-
-for(i in randomNumberStack){
-    if(i == randomChangeNumber){
+    for(i in randomNumberStack){
         if (randomNumberStack[i] % 3 == 0) {
-            createYellowDiv(secondPatternDiv); 
+            createRedDiv(firstPatternDiv); 
         } else {
-            createRedDiv(secondPatternDiv);
+            createYellowDiv(firstPatternDiv);
         }
     }
-    else{
-        if (randomNumberStack[i] % 3 == 0) {
-            createRedDiv(secondPatternDiv);
-        } else {
-            createYellowDiv(secondPatternDiv);
+
+    html2canvas(firstPatternDiv, {
+        onrendered: function (canvas) {
+            var a = document.createElement('a');
+            a.href = canvas.toDataURL("image/png");
+            a.download = n+'-pattern-1'+'.jpg';
+            a.click();
         }
+    });
+
+    var randomChangeNumber = Math.floor(Math.random() * gridNumber);
+
+    var secondPatternDiv = document.createElement("div");
+    secondPatternDiv.className="pattern";
+    secondPatternDiv.id="second-pattern-"+n;
+    document.body.appendChild(secondPatternDiv);
+
+    for(i in randomNumberStack){
+        if(i == randomChangeNumber){
+            if (randomNumberStack[i] % 3 == 0) {
+                createYellowDiv(secondPatternDiv); 
+            } else {
+                createRedDiv(secondPatternDiv);
+            }
+        }
+        else{
+            if (randomNumberStack[i] % 3 == 0) {
+                createRedDiv(secondPatternDiv);
+            } else {
+                createYellowDiv(secondPatternDiv);
+            }
+        }
+        
     }
-    
+
+    html2canvas(secondPatternDiv, {
+        onrendered: function (canvas) {
+            var a = document.createElement('a');
+            a.href = canvas.toDataURL("image/png");
+            a.download = n+'-pattern-2'+'.jpg';
+            a.click();
+        }
+    });
 }
 
-html2canvas(secondPatternDiv, {
-    onrendered: function (canvas) {
-        var a = document.createElement('a');
-        a.href = canvas.toDataURL("image/png");
-        a.download = 'test2.jpg';
-        a.click();
-    }
-});
 
